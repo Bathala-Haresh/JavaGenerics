9a691e7a940c2a548010239a384cc020fa7e826c
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-18 14:17:54 +0530

    added:program to extend max method to stdout

--- max.java
+++ max.java
@@ -1,28 +1,47 @@
 import java.util.*;
 
-public class findMax<T extends Comparable<T>> {
-	T[] elements;
+public class GenericsMax <T extends Comparable>{
+	//generics Array
+	private T[] inputArray;
 
-	public findMax(T[] elements) {
-		this.elements = elements;
+	public T[] getInputArray() {
+		return inputArray;
 	}
 
-	public static <T> T comparable(T[] elements) {
-		Arrays.sort(elements);
-		int length = elements.length;
-		T max = elements[length - 1];
-		return max;
+	public void setInputArray(T[] inputArray) {
+		this.inputArray = inputArray;
+	}
+
+	//method to find max of array of any data type
+	private static <T extends Comparable> void getMax(T[] array){
+		T max = array[0];
+		for (int i=0;i<array.length; i++){
+			if (array[i].compareTo(max)>0){
+				max=array[i];
+			}
+		}
+		GenericsMax.printMax(max);
+
+	}
+
+	//Extend the max method to print max 
+	private static<T> void printMax(T max) {
+		System.out.println("Maximum is : "+max);
 	}
 
 	public static void main(String[] args) {
-		Integer[] maxInt = { 1,12,123,1234,12345};
-		System.out.println("Maximum integer value is: " + comparable(maxInt));
+		// declaring array,double and float
+		Integer[] intArray={ 1,12,123,1234,12345};
+		Double[] doubleArray={1.2,1.23,12.3,123.123,0.123};
+		String[] stringArray={"Haresh","sourabh","vaishnavi","divya","ashish"};
 
-		Double[] maxDouble = {1.2,1.23,12.3,123.123,0.123 };
-		System.out.println("Maximum double value is: " + comparable(maxDouble));
+		System.out.println("maximum of integer value is :");
+		GenericsMax.getMax(intArray);
 
-		String[] maxString = { "Haresh","sourabh","vaishnavi","divya","ashish" };
-		System.out.println("Maximum string is: " + comparable(maxString));
+		System.out.println("maximum of Float value is:");
+		GenericsMax.getMax(doubleArray);
 
+		System.out.println("maximum of String value is:");
+		GenericsMax.getMax(stringArray);
 	}
 }
