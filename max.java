e9fae39309855d31e739fdf969b2ea88e5335288
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-18 14:36:27 +0530

    added:program to create generic class

--- max.java
+++ max.java
@@ -1,19 +1,55 @@
 
-public class refactor1 
-{
-	public static <T extends Comparable> T max(T... elements) {
-		T max = elements[0];
-		for (T element : elements) {
-			if (element.compareTo(max) > 0) {
-				max = element;
-			}
+import java.util.Scanner;
+
+public class refactor2<E extends Comparable> {
+	E x, y, z;
+
+	public refactor2(E x, E y, E z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static <T extends Comparable> T comparesion(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
 		}
 		return max;
 	}
 	public static void main(String[] args) {
-		System.out.println("Integer Max: " + max(234,12,567,34,234));
-		System.out.println("Double Max: " + max(1.2,5.6,7.8));
-		System.out.println("String Max: " + max("haresh","ramesh","suresh","dilip","chinni","rajesh"));
+		Scanner sc = new Scanner(System.in);
+		//float value from user
+		System.out.println("enter the float value:");
+		Float firstNumber = sc.nextFloat();
+		Float secondNumber = sc.nextFloat();
+		Float thirdNumber = sc.nextFloat();
+		
+		Float maxNumber = comparesion(firstNumber, secondNumber, thirdNumber);
+		System.out.println("Maximum value is:" + maxNumber);
+		System.out.println(" ");
+		//integer value form user
+		System.out.println("enter the integer value:");
+		Integer firstNumbers = sc.nextInt();
+		Integer secondNumbers = sc.nextInt();
+		Integer thirdNumbers = sc.nextInt();
+		
+		Integer maxNumbers = comparesion(firstNumbers, secondNumbers, thirdNumbers);
+		System.out.println("Maximum value is: " + maxNumbers);
+		System.out.println(" ");
+		//string value from user
+		System.out.println("enter the String:");
+		String firstString = sc.next();
+		String secondString = sc.next();
+		String thirdString = sc.next();
+		
+		String maxString = comparesion(firstString,secondString , thirdString);
+		System.out.println("Maximum value is:" + maxString);
+		sc.close();
+
 
 	}
 }
